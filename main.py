d64bde5f17991313677fb7ec687b626dfbbe74f3
Udi Cohen <udi.cohen@persimio.com>
2019-08-14 17:56:15 +0300

    adding GA class

--- main.py
+++ main.py
@@ -1,96 +1,80 @@
-import sys
 import operator
 import os
 import multiprocessing as mp
-import numpy as np
 from itertools import chain
-import shape as Shape
-import random
 from copy import deepcopy
 from population import Population
 from PIL import Image
 
-INITIAL_POPULATION = 20
-GENETAIONS_PER_IMAGE = 50
+INITIAL_POPULATION = 100
+GENETAIONS_PER_IMAGE = 6
 ADD_PER_ITER = 10
-MUTATION_CHANCE = 0.15
-CROSSOVER_CHANCE = 0.6
+SAVED_POPULATION = 0.1
+
 
 try:
     global_target = Image.open("target.png")
 except IOError:
     print("Can't found reference Image")
-    exit()
+    exit(1)
 
 
-def global_score(im1, im2):
-    i1 = np.array(im1, np.int16)
-    i2 = np.array(im2, np.int16)
-    dif = np.sum(np.abs(i1 - i2))
-    return (dif / 255.0 * 100) / i1.size
+class GA:
+    def __init__(self, target, cores):
+        self.target = target
+        self.cores = cores
+        self.generation = 1
+        self.parent = Population(target.size, INITIAL_POPULATION)
+        self.curr_img = self.parent.drawImage()
+        self.score = 100
 
+    def run_GA(self):
+        if not os.path.exists("results"):
+            os.mkdir("results")
 
-def mutateCrossOver(shapes):
-    for s in shapes:
-        case = np.random.random_sample()
-        if case <= MUTATION_CHANCE:
-            s.mutate()
-        elif case <= CROSSOVER_CHANCE:
-            s.crossover(random.choice(s.population.circles))
-    return shapes
+        f = open(os.path.join("results", "log.txt"), 'a')
 
+        pp = mp.Pool(self.cores)
 
-def create_nxt_generation(c, children_amount, p):
-    results = p.map(mutateCrossOver, [c]*int(children_amount))
-    result = list(chain(*results))
+        while True:
+        # print to console and log and save the picture every 50 iteration
+            print("Generation: #{}, score: {}".format(self.generation, self.score))
+            f.write("Generation: #{}, score: {}\n".format(self.generation, self.score))
 
-    return result
+            if self.generation % GENETAIONS_PER_IMAGE == 0:
+                self.parent.drawImage().save(os.path.join("results", "{}.png".format(generation)))
 
-def run_ga(cores):
-    if not os.path.exists("results"):
-        os.mkdir("results")
+            #score all shapes and sort them in by their fitness
+            self.parent.scoreCircles(self.curr_img, self.target)
+            self.parent.circles.sort(key=operator.attrgetter('fitness'))
 
-    f = open(os.path.join("results", "log.txt"), 'a')
-    target = global_target
-    generation = 1
-    parent = Population(target.size, INITIAL_POPULATION)
+            #take just the best 10%
+            self.parent.circles = self.parent.circles[:round(len(self.parent.circles) * SAVED_POPULATION)]
 
-    prev_score = 101
-    curr_img = parent.drawImage()
-    score = global_score(curr_img, target)
-
-    pp = mp.Pool(cores)
-
-    while True:
-            #print to console and log and save the picture every 50 iteration
-            print("Generation: #{}, score: {}".format(generation, score))
-            f.write("Generation: #{}, score: {}\n".format(generation, score))
-            if generation % GENETAIONS_PER_IMAGE == 0:
-                parent.drawImage().save(os.path.join("results", "{}.png".format(generation)))
-
-            """score all shapes and sort them in by their fitness"""
-            parent.scoreCircles(curr_img, target)
-            parent.circles.sort(key=operator.attrgetter('fitness'))
-
-            """take just the best 25%"""
-            parent.circles = parent.circles[:round(len(parent.circles) * 0.25)]
-
-            """start creating the next generation"""
-            generation += 1
-            children = deepcopy(parent)
+            #start creating the next generation
+            self.generation += 1
+            children = deepcopy(self.parent)
 
             try:
-                children.circles.extend(create_nxt_generation(children.circles, 5, pp))
+                children.circles.extend(self.create_nxt_generation(children.circles, ADD_PER_ITER, pp))
             except KeyboardInterrupt:
                 print("bye")
                 pp.close()
                 return
 
-            parent = deepcopy(children)
-            curr_img = parent.drawImage()
-            score = global_score(curr_img, target)
+            self.curr_img = children.drawImage()
+            self.score = children.global_score(self.curr_img, self.target)
+            self.parent = deepcopy(children)
+
+    def create_nxt_generation(self, c, children_amount, p):
+        results = p.map(self.parent.mutateCrossOver, [c]*int(children_amount))
+        result = list(chain(*results))
+
+        return result
+
 
 if __name__ == "__main__":
     cores = max(1, mp.cpu_count()-1)
 
-    run_ga(cores)
+    algo = GA(global_target, cores)
+    algo.run_GA()
--- population.py
+++ population.py
@@ -1,30 +1,24 @@
-import sys
-import os
 import numpy as np
-import matplotlib as plt
-from copy import deepcopy
 import random
 from PIL import Image
 from PIL import ImageDraw
-from shape import Shape
-import multiprocessing as mp
+from Circle import Circle
 
+#params
 MUTATION_CHANCE = 0.15
-CROSSOVER_CHANCE = 0.35
+CROSSOVER_CHANCE = 0.6
+
+
+""" Population class managing all the shapes in our picture
+    data fields: 
+    imgSz - target img size width*heighet
+    circles - all the circles in our image"""
 
 
 class Population:
     def __init__(self, imgSz, population_amount):
         self.imgSz = imgSz
-        self.circles = [Shape(imgSz, self) for _ in range(population_amount)]
-
-    def mutate_or_crossOver(self):
-
-        for s in random.sample(self.circles, int(len(self.circles) * MUTATION_CHANCE)):
-            s.mutate()
-
-        for s in random.sample(self.circles, int(len(self.circles) * CROSSOVER_CHANCE)):
-                s.crossover(random.sample(self.circles, 1))
+        self.circles = [Circle(imgSz, self) for _ in range(population_amount)]
 
     def scoreCircles(self, im1, im2):
         i1 = np.array(im1, np.int16)
@@ -34,29 +28,21 @@ class Population:
             c.score(dif)
 
     def drawImage(self):
-
         image = Image.new('RGB', self.imgSz, (255, 255, 255))
-        can = ImageDraw.Draw(image)
+        canvas = ImageDraw.Draw(image)
 
         for shapeIdx in self.circles:
             hue = (shapeIdx.color.r, shapeIdx.color.g, shapeIdx.color.b)
-            can.ellipse([shapeIdx.pos.x - shapeIdx.diameter, shapeIdx.pos.y - shapeIdx.diameter,
+            canvas.ellipse([shapeIdx.pos.x - shapeIdx.diameter, shapeIdx.pos.y - shapeIdx.diameter,
                                    shapeIdx.pos.x + shapeIdx.diameter, shapeIdx.pos.y + shapeIdx.diameter], outline=hue,
                                    fill=hue)
-
         return image
 
     def getSave(self, generation):
-        """
-        Allows us to save an individual organism in case the program is stopped.
-        """
         so = [generation]
         return so + [c.getSave() for c in self.circles]
 
     def loadSave(self, so):
-        """
-        Allows us to load an individual organism in case the program is stopped.
-        """
         self.circles = []
         gen = so[0]
         so = so[1:]
@@ -66,4 +52,17 @@ class Population:
             self.circles.append(newGene)
         return gen
 
+    def global_score(im1, im2):
+        i1 = np.array(im1, np.int16)
+        i2 = np.array(im2, np.int16)
+        dif = np.sum(np.abs(i1 - i2))
+        return (dif / 255.0 * 100) / i1.size
 
+    def mutateCrossOver(self, shapes):
+        for s in shapes:
+            case = np.random.random_sample()
+            if case <= MUTATION_CHANCE:
+                s.mutate()
+            elif case <= CROSSOVER_CHANCE:
+                s.crossover(random.choice(s.population.circles))
+        return shapes
